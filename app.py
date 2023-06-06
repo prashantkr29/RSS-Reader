598f01a2f3c6be76ef7809194cda75606ba426a1
prashant kumar <prashant.k2901@gmail.com>
2023-06-06 20:36:24 +0530

    fixed images

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 import requests
 import xml.etree.ElementTree as ET
+import feedparser
 
 app = Flask(__name__)
 
@@ -14,9 +15,11 @@ def index():
     # Parse XML data
     root = ET.fromstring(xml_data)
 
+    feed = feedparser.parse(url)
+
+
     # Initialize variables
     news_items = []
-
     # Iterate over each item in the XML feed
     for item in root.findall('.//item'):
         # Extract data from each item
@@ -24,20 +27,31 @@ def index():
         description = item.findtext('description')
         link = item.find('link').text
         category = item.find('category').text
-        image_url = item.find('media')
+       
+
+
         pubdate = item.find('pubDate').text
         pubdate = pubdate[:26]
 
-        print(image_url)
+
         # Append data to news_items list
         news_items.append({
             'title': title,
             'link': link,
             'description':description,
             'category':category,
-            'pubdate':pubdate,
-            'image':image_url
+            'pubdate':pubdate
         })
+     
+        
+
+    i=0
+    for entry in feed.entries:
+        image_url = entry.media_thumbnail[0]['url'] if 'media_thumbnail' in entry else None
+        #print(image_url)
+        news_items[i]['image_url'] = image_url
+        # print(news_items[i])
+        i+=1
 
     # Render the template with fetched data
     return render_template('index.html', news_items=news_items)
