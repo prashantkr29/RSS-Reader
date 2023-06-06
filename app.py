af0fbf5f361a310a2e982a5cbc36651e85e34e24
prashant kumar <prashant.k2901@gmail.com>
2023-06-06 18:44:56 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template
+import requests
+import xml.etree.ElementTree as ET
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    # Fetch XML data from wired.com
+    url = 'https://www.wired.com/feed/rss'
+    response = requests.get(url)
+    xml_data = response.text
+
+    # Parse XML data
+    root = ET.fromstring(xml_data)
+
+    # Initialize variables
+    news_items = []
+
+    # Iterate over each item in the XML feed
+    for item in root.findall('.//item'):
+        # Extract data from each item
+        title = item.find('title').text
+        description = item.findtext('description')
+        link = item.find('link').text
+        category = item.find('category').text
+        image_url = item.find('media:thumbnail')
+
+        # Append data to news_items list
+        news_items.append({
+            'title': title,
+            'link': link,
+            'description':description,
+            'category':category,
+            'image':image_url
+        })
+
+    # Render the template with fetched data
+    return render_template('index.html', news_items=news_items)
+
+if __name__ == '__main__':
+    app.run()
